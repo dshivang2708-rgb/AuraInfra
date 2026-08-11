8c3343febaf9ffa81d21467b7afbd520c60af546
Shivang <dshivang2002@gmail.com>
2026-08-11 11:31:44 +0530

    added route

--- src/router.jsx
+++ src/router.jsx
@@ -23,72 +23,72 @@ const homeRoute = createRoute({
 const aboutRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/about",
-}).lazy(() => import("./pages/About.lazy.jsx"));
+}).lazy(() => import("./pages/About.lazy.jsx").then((d) => d.Route));
 
 const contactRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/contact",
-}).lazy(() => import("./pages/Contact.lazy.jsx"));
+}).lazy(() => import("./pages/Contact.lazy.jsx").then((d) => d.Route));
 
 const residentialRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/properties/residential",
-}).lazy(() => import("./pages/ResidentialProperties.lazy.jsx"));
+}).lazy(() => import("./pages/ResidentialProperties.lazy.jsx").then((d) => d.Route));
 
 const residentialDetailRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/properties/residential/$slug",
-}).lazy(() => import("./pages/ResidentialProjectDetail.lazy.jsx"));
+}).lazy(() => import("./pages/ResidentialProjectDetail.lazy.jsx").then((d) => d.Route));
 
 const commercialRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/properties/commercial",
-}).lazy(() => import("./pages/CommercialProperties.lazy.jsx"));
+}).lazy(() => import("./pages/CommercialProperties.lazy.jsx").then((d) => d.Route));
 
 const commercialDetailRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/properties/commercial/$slug",
-}).lazy(() => import("./pages/CommercialProjectDetail.lazy.jsx"));
+}).lazy(() => import("./pages/CommercialProjectDetail.lazy.jsx").then((d) => d.Route));
 
 const agricultureRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/properties/agriculture",
-}).lazy(() => import("./pages/AgricultureProperties.lazy.jsx"));
+}).lazy(() => import("./pages/AgricultureProperties.lazy.jsx").then((d) => d.Route));
 
 const agricultureDetailRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/properties/agriculture/$slug",
-}).lazy(() => import("./pages/AgricultureProjectDetail.lazy.jsx"));
+}).lazy(() => import("./pages/AgricultureProjectDetail.lazy.jsx").then((d) => d.Route));
 
 const premiumProjectsRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/properties/premium-projects",
-}).lazy(() => import("./pages/PremiumProjects.lazy.jsx"));
+}).lazy(() => import("./pages/PremiumProjects.lazy.jsx").then((d) => d.Route));
 
 const premiumProjectDetailRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/properties/premium-projects/$slug",
-}).lazy(() => import("./pages/PremiumProjectDetail.lazy.jsx"));
+}).lazy(() => import("./pages/PremiumProjectDetail.lazy.jsx").then((d) => d.Route));
 
 const upcomingProjectsRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/properties/upcoming",
-}).lazy(() => import("./pages/UpcomingProjects.lazy.jsx"));
+}).lazy(() => import("./pages/UpcomingProjects.lazy.jsx").then((d) => d.Route));
 
 const adminSignInRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/admin/login",
-}).lazy(() => import("./pages/admin/AdminSignIn.lazy.jsx"));
+}).lazy(() => import("./pages/admin/AdminSignIn.lazy.jsx").then((d) => d.Route));
 
 const adminSignUpRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/admin/signup",
-}).lazy(() => import("./pages/admin/AdminSignUp.lazy.jsx"));
+}).lazy(() => import("./pages/admin/AdminSignUp.lazy.jsx").then((d) => d.Route));
 
 const adminDashboardRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/admin/dashboard",
-}).lazy(() => import("./pages/admin/AdminDashboard.lazy.jsx"));
+}).lazy(() => import("./pages/admin/AdminDashboard.lazy.jsx").then((d) => d.Route));
 
 const routeTree = rootRoute.addChildren([
   homeRoute,
